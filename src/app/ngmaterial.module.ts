10af489801067d16b15747755f9d1b5ec612419b
David Laganière <davidlag@gmail.com>
2018-12-01 14:54:25 -0500

    Addition of the table for transactions in the dataentries component.

--- src/app/ngmaterial.module.ts
+++ src/app/ngmaterial.module.ts
@@ -6,17 +6,26 @@ import { MatFormFieldModule } from '@angular/material';
 import { MatInputModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material';
+import { MatSortModule } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule,
             MatFormFieldModule,
             MatInputModule,
             MatCardModule,
-            MatSnackBarModule],
+            MatSnackBarModule,
+            MatTableModule,
+            MatPaginatorModule,
+            MatSortModule],
   exports: [MatButtonModule,
             MatFormFieldModule,
             MatInputModule,
             MatCardModule,
-            MatSnackBarModule]
+            MatSnackBarModule,
+            MatTableModule,
+            MatPaginatorModule,
+            MatSortModule]
 })
 export class MaterialAppModule { }
